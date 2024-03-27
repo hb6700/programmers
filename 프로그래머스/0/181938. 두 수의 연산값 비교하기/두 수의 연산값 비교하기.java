2ea81e7450027cbb231eacb447f75c7469f440b1
hb6700 <118810842+hb6700@users.noreply.github.com>
2024-03-27 23:52:04 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 1.18 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        int result1 = Integer.parseInt(String.valueOf(a) + String.valueOf(b));
+        int result2 = 2 * a * b;
+        
+        if(result1 >= result2){
+            answer = result1;
+        } else{
+            answer = result2;
+        }
+        return answer;
+    }
+}
