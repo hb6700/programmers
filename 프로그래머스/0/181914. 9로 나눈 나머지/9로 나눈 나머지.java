971e84e36a27fb4113ef01a0471bad993541489b
hb6700 <118810842+hb6700@users.noreply.github.com>
2024-04-04 23:51:26 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 0.02 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String number) {
+        int answer = 0, sum = 0;
+        for(int i=0 ; i<number.length() ; i++){
+            sum += number.charAt(i) - '0';
+        }
+        answer = sum % 9;
+        return answer;
+    }
+}
